401e03d2b4155c0dee383074be41e21234263b2c
João Paulo <joao.filho488@aluno.ce.gov.br>
2025-12-26 11:15:11 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const run = document.getElementById("cutucar")
-const mensagem = document.getElementById("mensagem")
-let pontos = 0
-
-run.addEventListener("click", () => {
-    pontos += 10
-    mensagem.innerText = "Pontos:" + pontos
-})
